1f92b37f28fc7743e09c970256972b21c7fa3a7f
torokoid <39209977+torokoid@users.noreply.github.com>
2024-03-14 13:02:07 +0900

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,16 @@
+<?php
+
+// 送信されたデータを取得
+$class_number = $_POST['class_number'];
+$name = $_POST['name'];
+$comment = $_POST['comment'];
+$attendance = $_POST['attendance'];
+
+// メール送信
+$to = 'satoshi_hada@jp.honda';
+$subject = '出欠確認';
+$message = "クラス名: {$class_number}\n名前: {$name}\nコメント: {$comment}\n出欠: {$attendance}";
+mail($to, $subject, $message);
+echo '送信が完了しました。';
+
+?>
