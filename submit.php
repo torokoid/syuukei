ee35c4cef6a629f9e83c60082a1bbbb174c8ea0d
torokoid <39209977+torokoid@users.noreply.github.com>
2024-03-14 14:51:17 +0900

    Add files via upload

--- submit.php
+++ submit.php
@@ -3,14 +3,19 @@
 // 送信されたデータを取得
 $class_number = $_POST['class_number'];
 $name = $_POST['name'];
-$comment = $_POST['comment'];
 $attendance = $_POST['attendance'];
 
 // メール送信
 $to = 'satoshi_hada@jp.honda';
 $subject = '出欠確認';
-$message = "クラス名: {$class_number}\n名前: {$name}\nコメント: {$comment}\n出欠: {$attendance}";
-mail($to, $subject, $message);
-echo '送信が完了しました。';
+$message = "クラス名: {$class_number}\n名前: {$name}\n出欠: {$attendance}";
+$headers = 'From: yourname@example.com' . "\r\n" .
+    'Reply-To: yourname@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
 
+if (mail($to, $subject, $message, $headers)) {
+    echo '送信が完了しました。';
+} else {
+    echo '送信に失敗しました。';
+}
 ?>
